49c2263e4f3dc08132975d6bd348e6b6426ed296
Ben Meier <ben.meier@humanitec.com>
2025-11-21 22:16:59 +0000

    fix: a few more descriptions

--- variables.tf
+++ variables.tf
@@ -1,19 +1,23 @@
 variable "resource-type-prefix" {
-  type    = string
-  default = ""
+  type        = string
+  default     = ""
+  description = "A prefix to add to the generated resource types"
 }
 
 variable "enable-score-workload" {
-  type    = bool
-  default = false
+  type        = bool
+  default     = false
+  description = "Whether to install the score-workload type"
 }
 
 variable "enable-k8s-namespace" {
-  type    = bool
-  default = false
+  type        = bool
+  default     = false
+  description = "Whether to install the k8s-namespace type"
 }
 
 variable "enable-k8s-service-account" {
-  type    = bool
-  default = false
+  type        = bool
+  default     = false
+  description = "Whether to install the k8s-service-account type"
 }
